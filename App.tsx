d5eb4325e1e3a8a3974bec69db22a48c731e1293
Bruno Parga <brunoparga@gmail.com>
2021-01-30 14:21:41 +0300

    Make app ready for CSV reading and writing

--- App.tsx
+++ App.tsx
@@ -1,25 +1,68 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { ScrollView, StyleSheet, Text } from 'react-native';
+import { Button } from 'react-native-elements';
+import * as FileSystem from 'expo-file-system';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    paddingTop: 22,
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+
+  title: {
+    fontSize: 32,
+    marginTop: 20,
+  },
+
+  buttonContainer: {
+    margin: 20,
+  },
+
+  button: {
+    height: 80,
+    paddingHorizontal: 20,
+  },
+
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
   },
 });
 
-function App(): JSX.Element {
+function App(): React.ReactNode {
+  const [times, setTimes] = useState([] as { awake: boolean; time: Date }[]);
+  const [amAwake, setAmAwake] = useState(true);
+  const currentState = amAwake ? 'awake' : 'asleep';
+
+  const timeElements = times.map(({ awake, time }) => (
+    <Text key={time.toISOString()} style={styles.item}>
+      {awake ? 'Awake' : 'Asleep'} at {time.toISOString()}
+    </Text>
+  ));
+
+  function onPress() {
+    const newAmAwake = !amAwake;
+
+    setAmAwake(newAmAwake);
+    setTimes(times.concat([{ awake: newAmAwake, time: new Date() }]));
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.title}>Sleep Log</Text>
+      <Button
+        containerStyle={styles.buttonContainer}
+        buttonStyle={styles.button}
+        onPress={onPress}
+        title="Add new time!"
+      />
+      <Text>Currently {currentState}</Text>
+      {timeElements}
+    </ScrollView>
   );
 }
 
-// Used only in the test and internally by Expo
 // eslint-disable-next-line import/no-unused-modules
 export default App;
